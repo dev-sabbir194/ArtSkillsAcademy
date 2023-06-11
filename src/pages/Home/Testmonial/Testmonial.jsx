68de1b4ec1f7cf3f93a62e0a0d52c7d9fe061cd3
unknown <hossainmdsabbir194@gmail.com>
2023-06-12 00:13:49 +0600

    testmonial

---
+++ src/pages/Home/Testmonial/Testmonial.jsx
@@ -0,0 +1,71 @@
+import  { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+
+const Testimonial = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
+  useEffect(() => {
+    const fetchTestimonials = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/reviews");
+        const data = await response.json();
+        setTestimonials(data);
+      } catch (error) {
+        console.log("Error fetching testimonials:", error);
+      }
+    };
+
+    fetchTestimonials();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTestimonial(
+        (prevIndex) => (prevIndex + 1) % testimonials.length
+      );
+    }, 4000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [testimonials]);
+
+  return (
+    <Container>
+      <div>
+        <h1 className="text-center mb-5 bg-black" id="header-text">
+          Testmonial
+        </h1>
+        <section className="testimonials-area section-gap">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-5">
+                <div className="item">
+                  <div className="testi-item">
+                    <img src="img/quote.png" alt="" />
+                    <div className="mt-40 text">
+                      <p>{testimonials[currentTestimonial]?.details}</p>
+                    </div>
+                    <h4>{testimonials[currentTestimonial]?.name}</h4>
+                    <p>{testimonials[currentTestimonial]?.position}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="offset-lg-1 col-lg-6">
+                <img
+                  className="img-fluid"
+                  src={testimonials[currentTestimonial]?.image}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Container>
+  );
+};
+
+export default Testimonial;
